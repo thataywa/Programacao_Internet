be585ba2ff723b6900fcf832e91ac7997a13e82a
thataywaa <thauanyeulina7@gmail.com>
2025-09-30 08:20:10 -0300

    metodo criar tarefa e listar tarefa feitos

--- PPI-Thauany/todo-list2.php
+++ PPI-Thauany/todo-list2.php
@@ -5,24 +5,39 @@ $usario = 'root';
 $senha = '';
 $database = 'todo_list'; 
 
-$conn = mysqli_connect($localhost,$usario,$senha, $database);
+$conn = new mysqli($localhost,$usario,$senha, $database);
 
 if($conn->connect_error){
     die('Deu erro na conexão'. $conn->connect_error);
 }
 
 # criacao de tarefas
-if(isset($_POST['descricao'])){
+if(isset($_POST['descricao'])&& !empty(trim($_POST['descricao']))){
     $descricao = $conn->real_escape_string($_POST['descricao']);
     $sqlInsert = "INSERT INTO tarefas (descricao) VALUES ('$descricao')"; 
+
+    if($conn->query($sqlInsert) === TRUE){
+        header("location todo-list2.php");
+    }
+
 }
+
+
 # Exclusão de tarefas
 
 
-$tarefas=[]; 
+
 # Listar tarefas
+$tarefas=[]; 
 
+$sqlSelect = "SELECT * FROM tarefas ORDER BY data_criacao DESC";
+$result = $conn->query($sqlSelect);
 
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        $tarefas[]=$row;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -42,7 +57,7 @@ $tarefas=[];
 
     <h2>Suas tarefas</h2>
     <?php if(!empty($tarefas)):?>
-    <h3>Suas tarefas</h3>
+    <h3>Minhas tarefas</h3>
     <?php else:?>
     <h3>Não tem tarefas</h3>
     <?php endif;?>
